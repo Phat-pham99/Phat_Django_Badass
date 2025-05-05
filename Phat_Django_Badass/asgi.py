380a0f61da35128684e275bd8ad9b3f3023695c6
Phat H. Pham <57822898+Phat-pham99@users.noreply.github.com>
2025-05-05 07:23:11 +0000

    Initial codebase

---
+++ Phat_Django_Badass/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Phat_Django_Badass project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Phat_Django_Badass.settings")
+
+application = get_asgi_application()
---
+++ Phat_Django_Badass/phat_finance/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhatFinanceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "phat_finance"
