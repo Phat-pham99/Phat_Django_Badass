33c0c5d289dec1b2c7c3f521b6734cdb32998293
Phat H. Pham <57822898+Phat-pham99@users.noreply.github.com>
2025-05-16 04:56:59 +0000

    Update Phat_Fitness

--- phat_fitness/admin.py
+++ phat_fitness/admin.py
@@ -3,7 +3,7 @@ from .models.gym_track import GymTrack
 
 # Register your models here.
 class GymTrackAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date', 'start', 'end', 'routine')
+    list_display = ('user', 'date', 'start', 'end', 'duration', 'routine')
     search_fields = ('user', 'date', 'routine')
     list_filter = ('user', 'date', 'routine')
     ordering = ('-date',)
--- phat_fitness/models/gym_track.py
+++ phat_fitness/models/gym_track.py
@@ -21,14 +21,17 @@ class GymTrack(models.Model):
     date = models.DateField()
     start = models.DateTimeField(default=django.utils.timezone.now)
     end = models.DateTimeField(default=django.utils.timezone.now)
-    duration = models.DurationField(editable=False)
+    duration = models.CharField(editable=False,default='0')
     routine = models.CharField(max_length=100, choices=ROUTINE_CHOICES, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         # Calculate the duration when saving the instance
         if self.start and self.end:
             # Ensure that the duration is a timedelta
-            self.duration = self.end_time - self.start_time
+            self.duration = str(self.end - self.start)
+            print("start",self.start)
+            print("end",self.end)
+            print("duration",self.end - self.start,type(self.end - self.start))
         else:
             self.duration = timedelta(0)  # Default to zero duration if times are not set
         super().save(*args, **kwargs)
