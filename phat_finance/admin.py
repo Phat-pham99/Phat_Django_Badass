4a0c0fe49604c157d6d4242e402cbca02f892479
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2025-05-23 00:21:11 +0700

    Salary pay fucntion worked

--- phat_finance/admin.py
+++ phat_finance/admin.py
@@ -5,6 +5,8 @@ from .models.assets import Assets
 from .models.balances import Balance
 from .models.emergency_funds import EmergencyFund
 from .models.sinking_funds import SinkingFund
+from .models.in_out_flows import InOutFlow
+
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('date','user','cash','digital','credit','category', 'description')
     search_fields = ('date', 'category', 'description')
@@ -24,15 +26,24 @@ class EmergencyFundAdmin(admin.ModelAdmin):
     list_display = ('date','type','amount')
 
 class SinkingFundAdmin(admin.ModelAdmin):
-    list_display = ('date','type','amount')  
+    list_display = ('date','type','amount')
+
+class InOutFlowAdmin(admin.ModelAdmin):
+    list_display = ['date','placeholder','amount']
 
 class BalanceAdmin(admin.ModelAdmin):
+    list_display = ["current_month",'expense','debt',
+                    'investment','asset','cash','digital',
+                    'emergency_fund','sink_fund','networth'
+                    ]
     fields = [('cash','digital'),('debt','asset'),'investment',
             ('emergency_fund','sink_fund')]
     readonly_fields = ['current_month','expense','networth']
+
 admin.site.register(Expense, ExpenseAdmin)
-# admin.site.register(Debts,DebtAdmin)
-# admin.site.register(EmergencyFund,EmergencyFundAdmin)
-# admin.site.register(SinkingFund,SinkingFundAdmin)
-# admin.site.register(Assets,AssetAdmin)
-# admin.site.register (Balance, BalanceAdmin)
+admin.site.register(Debts,DebtAdmin)
+admin.site.register(InOutFlow,InOutFlowAdmin)
+admin.site.register(EmergencyFund,EmergencyFundAdmin)
+admin.site.register(SinkingFund,SinkingFundAdmin)
+admin.site.register(Assets,AssetAdmin)
+admin.site.register (Balance, BalanceAdmin)
--- phat_finance/models/balances.py
+++ phat_finance/models/balances.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import date
 from .debts import Debts
 from phat_investment.models.investment import Investment
 from phat_finance.models.in_out_flows import InOutFlow
 from .assets import Assets
 from .emergency_funds import EmergencyFund
 from .sinking_funds import SinkingFund
+
 
 class BalanceManager(models.Manager):
     def add(self,balance,cash,digital):
@@ -23,13 +25,22 @@ class BalanceManager(models.Manager):
         balance.save()
         return balance
 
-    @classmethod
+    # @classmethod
     def salary_paid(self,amount):
     # Yeah I know this is some shitty codes, but I will try to improve overtime
         balance, _ = self.get_or_create()
         self.add(balance,0,amount)
-        InOutFlow_obj = InOutFlow.create(_type="IN",placeholder=amount)
+        InOutFlow_obj = InOutFlow.objects.create(_type="IN",placeholder=amount)
         InOutFlow_obj.save()
+
+    def invest(self,date,investment_type,amount):
+        balance, _ = self.get_or_create(self)
+        self.minus(balance,0,amount)
+        Investment_obj = Investment.objects.create(date=date,
+                        investment_type=investment_type,
+                        amount=amount
+                        )
+        Investment_obj.save()
 
 
 class Balance(models.Model):
@@ -48,13 +59,7 @@ class Balance(models.Model):
 
     objects = BalanceManager()
 
-    # def save(self, *args, **kwargs):
-    #     self.expense_id = self.expense.id
-    #     self.debt_id = self.debt.id
-    #     self.investment_id = self.investment.id
-    #     self.asset_id = self.asset.id
-    #     self.emergency_fund_id = self.emergency_fund.id
-    #     self.sink_fund_id = self.sink_fund.id
-    #     self.networth = -1*self.debt.amount + self.investment.amount + self.asset.amount \
-    #         + self.cash + self.digital + self.emergency_fund.amount + self.sink_fund.amount
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.networth = -1*self.debt + self.investment + self.asset \
+            + self.cash + self.digital + self.emergency_fund + self.sink_fund
+        super().save(*args, **kwargs)
