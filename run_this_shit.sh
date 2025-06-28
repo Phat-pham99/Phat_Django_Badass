0e5092743cb2036bcfc47b07d2fcd5f3eb06e8f9
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2025-06-28 20:23:19 +0700

    Minor fixes

--- run_this_shit.sh
+++ run_this_shit.sh
@@ -1,3 +1,3 @@
-# eval "$(poetry env activate)"
-source /home/codespace/.cache/pypoetry/virtualenvs/phat-django-badass-Y9-ht7DM-py3.12/bin/activate
-gunicorn Phat_Django_Badass.wsgi:application --threads 5 --bind 0.0.0.0:8000
+eval "$(poetry env activate)"
+hypercorn Phat_Django_Badass.asgi:application --reload -b 0.0.0.0:8000 --access-logfile Django_log.log
+
