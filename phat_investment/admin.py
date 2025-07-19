198f306d46e0525567311e2ac17a1ddf8e63464c
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2025-07-19 11:12:14 +0700

    DividendAdmin update some

--- phat_investment/admin.py
+++ phat_investment/admin.py
@@ -10,7 +10,7 @@ from .models.dividend import Dividend_new
 @admin.register(Investment)
 class InvestmentAdmin(admin.ModelAdmin):
     list_display = ('date','investment_type','amount')
-    search_fields = ('date','investment_type')
+    search_fields = ('date',)
     list_filter = ('date','investment_type')
 
 @admin.register(TrackInvestment)
@@ -21,4 +21,16 @@ class TrackInvestmentAdmin(admin.ModelAdmin):
 @admin.register(Dividend_new)
 class DividendAdmin(admin.ModelAdmin):
     list_display = ('secCd','rightType','ownQty','recAmt','recQty','ownerFixDate','expectedExcDate')
-    list_filter = ('ownerFixDate','rightType')
+    list_filter = ('ownerFixDate','rightType','secCd')
+    
+    @admin.display(empty_value=0)
+    def recAmt(self, obj):
+        return obj.recAmt
+    
+    @admin.display(empty_value=0)
+    def recQty(self, obj):
+        return obj.recQty
+    
+    @admin.display(empty_value='Dunno bruh')
+    def ownerFixDate(self, obj):
+        return obj.ownerFixDate
