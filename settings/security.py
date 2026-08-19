12c6d13e0bd41be82abcfc980ec41ed7c8866ae8
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2026-08-19 19:47:32 +0700

    Allow origin ALL

--- settings/security.py
+++ settings/security.py
@@ -1,9 +1,6 @@
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 AUTH_PASSWORD_VALIDATORS = [
     {
