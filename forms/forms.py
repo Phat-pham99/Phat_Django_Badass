d7fa97c8c50e476f6b991567fbac82f8fd7739e5
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2026-08-20 22:42:52 +0700

    Remove old Redis Form

--- forms/forms.py
+++ forms/forms.py
@@ -13,20 +13,3 @@ class DateFilterForm(forms.Form):
         label="End date", widget=forms.DateInput(attrs={"type": "date"}), required=False
     )
 
-
-class RedisDataForm(forms.Form):
-    balance_cash = forms.IntegerField(label="Balance cash 💷",
-      widget=forms.TextInput(attrs={"Balance cash 💷": "Enter an integer"}),
-      required=False)
-
-    balance_digital = forms.IntegerField(label="Balance digital 🏧",
-      widget=forms.TextInput(attrs={"Balance digital 🏧": "Enter an integer"}),
-      required=False)
-
-    balance_credit = forms.IntegerField(label="Balance credit 💳",
-      widget=forms.TextInput(attrs={"Balance credit 💳": "Enter an integer"}),
-      required=False)
-
-    necessity = forms.IntegerField(label="Necessity ",
-      widget=forms.TextInput(attrs={"Necessity ": "Enter an integer"}),
-      required=False)
