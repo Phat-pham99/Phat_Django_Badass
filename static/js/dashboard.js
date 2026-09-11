4efeada617150ac5fb62411a966806e3be12cfb8
Phat-pham99 <57822898+Phat-pham99@users.noreply.github.com>
2026-09-11 23:02:55 +0700

    Minor fixes

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,16 +1,17 @@
 function main(necessity, pleasure, saving_month, investment_month, rent, vacation, funds, cashflow, expensable, expense_cash, expense_digital, expense_credit) {
+    const remaining = parseInt(expensable) -
+    (parseInt(expense_cash) + parseInt(expense_digital) 
+    + parseInt(expense_credit));
 
-  const remaining = parseInt(expensable) -
-  (parseInt(expense_cash) + parseInt(expense_digital) + parseInt(expense_credit));
-  if (remaining < 0) {
-  window.alert("Expensable exceeded");
-  }
-  const xDoughnutExpense = ["expense_cash", "expense_digital", "expense_credit", "remaining"];
-  const yDoughnutExpense = [ expense_cash,expense_digital, expense_credit, remaining ];
-  const DoughnutExpenseColors = ["#ff96a4", "#ccffcc","#96bfff"];
-  const xDoughnutBudget = ["necessity", "pleasure","saving_month", "investment_month",
-                  "rent", "vacation", "funds", "cashflow"]
-  const yDoughnutBudget = [
+    if (remaining < 0) {
+    window.alert("Expensable exceeded");
+    }
+    const xDoughnutExpense = ["expense_cash", "expense_digital", "expense_credit", "remaining"];
+    const yDoughnutExpense = [ expense_cash,expense_digital, expense_credit, remaining ];
+    const DoughnutExpenseColors = ["#ff96a4", "#ccffcc","#96bfff"];
+    const xDoughnutBudget = ["necessity", "pleasure","saving_month", "investment_month",
+                "rent", "vacation", "funds", "cashflow"]
+    const yDoughnutBudget = [
     necessity,
     pleasure,
     saving_month,
@@ -19,79 +20,79 @@ function main(necessity, pleasure, saving_month, investment_month, rent, vacatio
     vacation,
     funds,
     cashflow
-  ]
-  const DoughnutBudgetColors = [
-    "#ff2929",
-    "#0047c9",
-    "#69ff7a",
-    "pink",
-    "#ff8e4d",
-    "#73e3ff",
-    "#9e3dff",
-    "#fff878"
-  ]
+    ]
+    const DoughnutBudgetColors = [
+        "#ff2929",
+        "#0047c9",
+        "#69ff7a",
+        "pink",
+        "#ff8e4d",
+        "#73e3ff",
+        "#9e3dff",
+        "#fff878"
+    ]
 
-  new Chart("DoughnutExpense", {
-  type: "doughnut",
-  data: {
-  labels: xDoughnutExpense,
-  datasets: [{
-  backgroundColor: DoughnutExpenseColors,
-  data: yDoughnutExpense
-  }]
-  },
-  options: {
-  legend: {
-      display: true,
-      position: "right"
-  },
-  tooltips: {
-      callbacks: {
-          label: function(tooltipItem, data) {
-              var label = data.labels[tooltipItem.index] || '';
-              if (label) {
-                  label += ': ';
-              }
-              var value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
-              label += value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
-              " (" + (value / parseInt(expensable) * 100).toFixed(2) + " %)";
-              return label;
-          }
-      }
-  }
-  }
-  });
+    new Chart("DoughnutExpense", {
+    type: "doughnut",
+    data: {
+    labels: xDoughnutExpense,
+    datasets: [{
+    backgroundColor: DoughnutExpenseColors,
+    data: yDoughnutExpense
+    }]
+    },
+    options: {
+    legend: {
+        display: true,
+        position: "right"
+    },
+    tooltips: {
+        callbacks: {
+            label: function(tooltipItem, data) {
+                var label = data.labels[tooltipItem.index] || '';
+                if (label) {
+                    label += ': ';
+                }
+                var value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                label += value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                " (" + (value / parseInt(expensable) * 100).toFixed(2) + " %)";
+                return label;
+            }
+        }
+    }
+    }
+    });
 
-  new Chart("DoughnutBudget", {
-  type: "doughnut",
-  data: {
-  labels: xDoughnutBudget,
-  datasets: [{
-  backgroundColor: DoughnutBudgetColors,
-  data: yDoughnutBudget
-  }]
-  },
-  options: {
-  legend: {
-      display: true,
-      position: "right"
-  },
-  tooltips: {
-      callbacks: {
-          label: function(tooltipItem, data) {
-              var label = data.labels[tooltipItem.index] || '';
-              if (label) {
-                  label += ': ';
-              }
-              var value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
-              label += value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
-              " (" + (value / parseInt("{{ budget | safe }}") * 100).toFixed(2) + " %)";
-              return label;
-          }
-      }
-  }
-  }
-  });
+    new Chart("DoughnutBudget", {
+    type: "doughnut",
+    data: {
+    labels: xDoughnutBudget,
+    datasets: [{
+    backgroundColor: DoughnutBudgetColors,
+    data: yDoughnutBudget
+    }]
+    },
+    options: {
+    legend: {
+        display: true,
+        position: "right"
+    },
+    tooltips: {
+        callbacks: {
+            label: function(tooltipItem, data) {
+                var label = data.labels[tooltipItem.index] || '';
+                if (label) {
+                    label += ': ';
+                }
+                var value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                label += value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                " (" + (value / parseInt(budget) * 100).toFixed(2) + " %)";
+                return label;
+            }
+        }
+    }
+    }
+    });
 }
 
 function toggleText(elementId) {
